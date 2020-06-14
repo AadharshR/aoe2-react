84021b1bbc1f1f3d1db115ab868a273af15901d9
AadharshR <aadharshr6@gmail.com>
2020-06-14 19:17:25 +0530

    nav bar css design in progress

--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import style from "./Navbar.css";
+import "./Navbar.css";
 import Loader from "./Loader";
 import Dropdown from "react-dropdown";
 import {
@@ -8,7 +8,9 @@ import {
   Navbar,
   NavDropdown,
   MenuItem,
-  NavItem
+  NavItem,
+  ListGroup,
+  Card
 } from "react-bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
@@ -19,16 +21,23 @@ class SideNavbar extends Component {
   state = {
     civs: [],
     loading: true,
-    isOpen: false
+    isOpen: false,
+    isExpanded: false
   };
   handleOpen = () => {
     this.setState({ isOpen: true });
+    console.log("here");
   };
 
   handleClose = () => {
     this.setState({ isOpen: false });
   };
-
+  handleToggle(e) {
+    e.preventDefault();
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
   async componentDidMount() {
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
     const url = "https://aoe2.net/api/strings?game=aoe2de&language=en";
@@ -41,38 +50,27 @@ class SideNavbar extends Component {
     this.setState({ civs: json.civ, loading: false });
   }
   onClick = async e => {};
+
   render() {
     return (
       <div>
         {this.state.loading ? <Loader /> : null}
-        <Navbar bg="light" expand="lg">
+        <Navbar className="side-nav" scrollable={true} bg="light" expand="lg">
           <Nav
             className="col-md-12 d-none d-md-block bg-light sidebar"
             activeKey="/home"
             onSelect={selectedKey => alert(`selected ${selectedKey}`)}
           >
-            <Nav pullRight>
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Router>
-                  <LinkContainer to="/home">
-                    <NavItem href="/home" eventKey={1}>
-                      CIVILIZATIONS
-                    </NavItem>
-                  </LinkContainer>
-                </Router>
-              </Navbar.Collapse>
-            </Nav>
-            <NavDropdown
-              onMouseEnter={this.handleOpen}
-              onMouseLeave={this.handleClose}
-              open={this.state.isOpen}
-              noCaret
-              id="language-switcher-container"
-            >
-              <div className="sidebar-sticky" />
+            <NavDropdown title="CIVILIZATIONS">
               {this.state.civs.map(civ => (
-                <NavDropdown.Item id={civ.id} className="card-title">
-                  {civ.string}
+                <NavDropdown.Item className="side-nav" id={civ.id}>
+                  <Card style={{ width: "18rem" }}>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item> {civ.string}</ListGroup.Item>
+                    </ListGroup>
+                  </Card>
+
+                  <br />
                 </NavDropdown.Item>
               ))}
             </NavDropdown>
