d8e5d801b3951fa7269da0d4e7729770bda588ad
AadharshR <aadharshr6@gmail.com>
2020-08-12 23:50:05 +0530

    civ logo added for half of the civs and design adjustments made

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import SideNavbar from "./views/SideNavbar";
 import MainPage from "./views/MainPage";
 import { Container, Row, Col } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import "./assets/MainPage.css"
 
 class App extends Component {
 
   render() {
     return (
+      <div className ="bg-image">
         <Router>
         <Container fluid>
               <Row>
-              <Col xs={2} id="sidebar-wrapper">      
+              <Col lg={2} id="sidebar-wrapper">      
                       <SideNavbar />
                 </Col>
-                <Route path = "/civs/:id" component = {()=> (<Col xs = {10}><MainPage/></Col>)}/>
+                <Route path = "/civs/:id" component = {()=> (<Col lg = {10}><MainPage/></Col>)}/>
                   
                 </Row>
 
             </Container>
         </Router>
-
+   
+        </div>
           
     );
   }
--- src/views/MainPage.js
+++ src/views/MainPage.js
@@ -1,49 +1,51 @@
 import "../assets/MainPage.css";
 
 import React, { useEffect, useState } from "react";
-import {   
-    useParams
-  } from "react-router-dom";
+import {
+  useParams
+} from "react-router-dom";
 
- 
-
-
-const  MainPage = () => {
+const MainPage = () => {
   const initialValue = []
-  const [civs,setCivs] = useState(initialValue)
-    useEffect(()=> {
-      async function fetchData() {
-        let result = await (await fetch(`${process.env.REACT_APP_HOST_URL}/api/civs/`)).json()    
-        setCivs(result)
-      }
-      fetchData();
-    },[])
-    let { id } = useParams();
-    console.log("civs")
-     let ans = civs.map(value => {
-      if(value.civ === id)
-      {
-        console.log()
-       return (
-       <div className = "content">
-         
-         <br/>Civilization:{value.civ}
+  const [civs, setCivs] = useState(initialValue)
+  useEffect(() => {
+    async function fetchData() {
+      let result = await (await fetch(`${process.env.REACT_APP_HOST_URL}/api/civs/`)).json()
+      setCivs(result)
+    }
+    fetchData();
+  }, [])
+  let { id } = useParams();
+  let ans = civs.map(value => {
+    if (value.civ === id)
 
-         <div>
+      return (
+        <div className="content">
 
-         </div>
+          <img src={require(`../assets/civs/CivIcon-${value.civ}.png`)} alt="logo" />
+          <br />CIVILIZATION:{value.civ}
+          <br></br>
+          IDEAL OPENING: {value.ideal_opening}<br />
+          FEUDAL OPENING :{value.transition} <br />
+          CASTLE AGE TRANSITION: {value.castle_age}
+          <br />
+          IMPERIAL AGE: {value.imperial_age}
+          <br />
+          <div>
+
+          </div>
         </div>)
-      
-     }})
-    console.log("ans",ans)
-    return (
-      
-      <div className = "bg-image">
 
-         {ans}
-  
-      </div>
-    );
+  }})
+  console.log("ans", ans)
+return (
+
+  <div>
+
+    {ans}
+
+  </div>
+);
   }
 
 export default MainPage;
